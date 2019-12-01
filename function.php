2cf4c066e8f793f2cf5df5c170cf748001f80ed6
centerRyo <abc.atrn@gmail.com>
2019-12-01 19:28:14 +0900

    記事一覧を更新日順に取ってくる

--- function.php
+++ function.php
@@ -210,7 +210,7 @@ function getRonbunList($currentMinNum = 1, $span = 12) {
     if (!$stmt) {
       return false;
     }
-    $sql = 'SELECT r.id, r.title, r.abstract, r.detail, r.image, r.created_date, r.updated_date, c.name AS category FROM ronbun AS r LEFT JOIN category AS c ON r.category_id = c.id WHERE r.delete_flg = 0 AND c.delete_flg = 0';
+    $sql = 'SELECT r.id, r.title, r.abstract, r.detail, r.image, r.created_date, r.updated_date, c.name AS category FROM ronbun AS r LEFT JOIN category AS c ON r.category_id = c.id WHERE r.delete_flg = 0 AND c.delete_flg = 0 ORDER BY r.updated_date DESC';
     $sql .= ' LIMIT ' . $span . ' OFFSET ' . $currentMinNum;
     $data = array();
     debug('SQL:'.$sql);
