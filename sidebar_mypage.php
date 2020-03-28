042a439573460b7e783e96ad6df214a7f88306c0
centerRyo <abc.atrn@gmail.com>
2020-03-28 22:51:27 +0900

    マイページにサイドバーを追加

---
+++ sidebar_mypage.php
@@ -0,0 +1,6 @@
+<section class="sidebar-mypage">
+  <a href="profEdit.php">プロフィールを編集する</a>
+  <a href="registerRonbun.php">論文を投稿する</a>
+  <a href="passEdit.php">パスワードを変更する</a>
+  <a href="withdraw.php">退会する</a>
+</section>
